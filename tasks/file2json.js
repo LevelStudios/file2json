692007f0751e7ac3e87b3ac8053d6920a2f7c584
joelkelly <joel.kelly@me.com>
2015-03-10 15:50:49 -0700

    Refactor and V2.0

--- tasks/file2json.js
+++ tasks/file2json.js
@@ -9,148 +9,51 @@
 'use strict';
 
 module.exports = function(grunt) {
-    var src;
-    var dest;
-    var priority;
-    var template;
-    var filepath;
-
-    grunt.registerMultiTask('file2json', 'Parses file names of a specific format into a  JSON file', function() {
-
-        src                 = this.data.src;
-        dest                = this.data.dest;
-        priority            = this.data.priority;
-        template            = this.data.template.split("-");
-        filepath            = this.data.filepath;
-        var JSONstring;
-        var writtenCount    = 0;
-        var filenames       = [],
-            hpfilenames     = [],
-            fnSubs          = [],
-            hpfnSubs        = [];
-        var index           = src.indexOf('/');
-        var relative        = src.substring(index, src.length);
-
-        grunt.file.recurse(src, function(abspath, rootdir, subdir, filename){
-            var suffix      = filename.substring(filename.length - 3, filename.length);
-
-            if(suffix === ".md"){
-                if(subdir === priority){
-                    hpfilenames.push(filename);
-                    hpfnSubs.push(subdir);
-                }else {
-                    filenames.push(filename);
-                    fnSubs.push(subdir);
-                }
-            }
-        });
-
-        for(var i = 0; i < template.length; i++){
-            template[i] = '\t"' + template[i] + '": "';
-        }
-
-        JSONstring = "[\n{\n";
-
-        for(var i = 0; i < filenames.length; i++){
-            var templateCopy = [];
-
-            for(var j = 0; j < template.length; j++){
-                templateCopy[j] = template[j];
-            }
-            var slug = filenames[i].replace(/([\-])/g,'_').replace(/\.(?!md)/g,'-').replace(/\.md/g, '').replace(/^[a-z\-]+_/g, '');
-            var parts = filenames[i].split("-");
-            for(var j = 0; j < templateCopy.length; j++){
-                if(parts.length == templateCopy.length){
-                    if(j >= templateCopy.length - 1){
-                      parts[j] = parts[j].substr(0,parts[j].length - 3);
-                    }
-                    if(parts[j].indexOf('+') != -1){
-                        var plusHandler = parts[j].split('+');
-                        templateCopy[j] += toTitleCase(plusHandler[0].replace(/([.])/g,' ')) + " - " + plusHandler[1] + '",\n';
-                    } else {
-                        templateCopy[j] += toTitleCase(parts[j].replace(/([.])/g,' ')) + '",\n';
-                    }
-                    JSONstring += templateCopy[j];
-
-                }
-            }
-            if(parts.length == templateCopy.length){
-                if(filepath){
-                    JSONstring += '\t"filepath": "'  + relative + "/" + fnSubs[i] + "/" + filenames[i] + '",\n';
-                }
-
-                JSONstring += '\t"slug": "'  + slug + '"\n';
-
-                writtenCount++;
-                JSONstring +=  '}';
-                if(i != filenames.length - 1){
-                    JSONstring += ",\n{";
-                }else {
-                    JSONstring += "\n";
-                }
-            }
-        }
 
 
+    var toTitleCase = function(str){
+        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
 
-        if(hpfilenames.length > 1){
-          JSONstring += ",\n{";
-        }
+    grunt.registerMultiTask('file2json', 'Parses file names of a specific format into a JSON file', function() {
 
-        for(var i = 0; i < hpfilenames.length; i++){
-            var templateCopy = [];
-            for(var j = 0; j < template.length; j++){
-                templateCopy[j] = template[j];
-            }
-            var slug = hpfilenames[i].replace(/([\-])/g,'_').replace(/\.(?!md)/g,'-').replace(/\.md/g, '').replace(/^[a-z\-]+_/g, '');
-            var parts = hpfilenames[i].split("-");
-            for(var j = 0; j < templateCopy.length; j++){
-                if(parts.length == templateCopy.length){
-                    if(j >= templateCopy.length - 1){
-                      parts[j] = parts[j].substr(0,parts[j].length - 3);
-                    }
-                    if(parts[j].indexOf('+') != -1){
-                        var plusHandler = parts[j].split('+');
-                        templateCopy[j] += toTitleCase(plusHandler[0].replace(/([.])/g,' ')) + " - " + plusHandler[1] + '",\n';
-                    } else {
-                        templateCopy[j] += toTitleCase(parts[j].replace(/([.])/g,' ')) + '",\n';
-                    }
-                    JSONstring += templateCopy[j];
+        var JSONstring      = [];
+        var isFilepath      = this.data.filepath || false;
+        var isSlug          = this.data.slug || false
+        var priority        = this.data.priority;
+        var dest            = this.data.dest;
+        var template        = this.data.template.split("-");
+        var newPath         = this.data.newpath;
 
-                }
-            }
-            if(parts.length == templateCopy.length){
-                JSONstring += '\t"priority": true,\n';
-                if(filepath){
-                    JSONstring += '\t"filepath": "'  + relative + "/" + hpfnSubs[i] + "/" + hpfilenames[i] + '",\n';
-                }
+        this.files.map( function ( fileArray ) {
+            var src = fileArray.src;
 
-                JSONstring += '\t"slug": "'  + slug + '"\n';
+            src.map(function( file ){
+                console.log(file);
 
-                writtenCount++;
-                JSONstring +=  '}';
-                if(i != hpfilenames.length - 1){
-                    JSONstring += ",\n{";
-                }else {
-                    JSONstring += "\n";
-                }
-            }
-        }
+                // Is this a priority?
+                var directory       = file.substring(0, file.indexOf('/'));
+                var isPriority      = (directory.toLowerCase() === priority.toLowerCase());
 
-        JSONstring += "]";
-        if(writtenCount == 0){
-            JSONstring = "";
-        }else if(writtenCount == 1){
-            JSONstring = JSONstring.substr(0, JSONstring.length - 4);
-            JSONstring += "\n]";
-        }
-        grunt.file.write(dest, JSONstring);
+                var fileName        = file.substring(file.indexOf('/')+1).replace('.md', '')
+                var fileParts       = fileName.split("-");
+                var slug            = fileName.replace(/([\-])/g,'_').replace(/(\.)/g, '-');
 
-        //Helper function that formats names to proper first character after space upper case
-        function toTitleCase(str) {
-            return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-        }
+                var JSONfragment    = {};
 
+                if (isFilepath) JSONfragment['filepath']    = newPath + file;
+                if (isSlug)     JSONfragment['slug']        = slug;
+                if (isPriority) JSONfragment['priority']    = isPriority;
+
+                for (var i = 0; i < template.length; i++) {
+                   JSONfragment[ template[i] ] = toTitleCase(fileParts[i].replace(/(\.)/g, ' '));
+                };
+
+                JSONstring.push(JSONfragment);
+            })
+        })
+
+        grunt.file.write(dest, JSON.stringify(JSONstring));
     });
 
 };
