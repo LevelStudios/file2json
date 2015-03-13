4b21000185457ed5c3a5a89bb47e83e8fd7daee0
joelkelly <joel.kelly@me.com>
2015-03-13 11:27:30 -0700

    Enhancing toTitleCase with exceptions
    Brute force on this one. Should be more elegant

--- tasks/file2json.js
+++ tasks/file2json.js
@@ -12,14 +12,24 @@ module.exports = function(grunt) {
 
 
     var toTitleCase = function(str){
-        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    }
+        var converted = str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+            converted = converted.replace(/Ios\s/g, 'iOS ');
+            converted = converted.replace(/Ux\s/g, 'UX ');
+            converted = converted.replace(/Ui\s/g, 'UI ');
+            converted = converted.replace(/Qa\s/g, 'QA ');
+            converted = converted.replace(/Gp\s/g, 'GP ');
+            converted = converted.replace(/Pd\s/g, 'PD ');
+            converted = converted.replace(/Iphone/g, 'iPhone ');
+            converted = converted.replace(/IPad/g, 'iPad ');
+
+        return converted;
+    };
 
     grunt.registerMultiTask('file2json', 'Parses file names of a specific format into a JSON file', function() {
 
         var JSONstring      = [];
         var isFilepath      = this.data.filepath || false;
-        var isSlug          = this.data.slug || false
+        var isSlug          = this.data.slug || false;
         var priority        = this.data.priority;
         var dest            = this.data.dest;
         var template        = this.data.template.split("-");
@@ -29,13 +39,12 @@ module.exports = function(grunt) {
             var src = fileArray.src;
 
             src.map(function( file ){
-                console.log(file);
 
                 // Is this a priority?
                 var directory       = file.substring(0, file.indexOf('/'));
                 var isPriority      = (directory.toLowerCase() === priority.toLowerCase());
 
-                var fileName        = file.substring(file.indexOf('/')+1).replace('.md', '')
+                var fileName        = file.substring(file.indexOf('/')+1).replace('.md', '');
                 var fileParts       = fileName.split("-");
                 var slug            = fileName.replace(/([\-])/g,'_').replace(/(\.)/g, '-');
 
@@ -47,11 +56,11 @@ module.exports = function(grunt) {
 
                 for (var i = 0; i < template.length; i++) {
                    JSONfragment[ template[i] ] = toTitleCase(fileParts[i].replace(/(\.)/g, ' '));
-                };
+                }
 
                 JSONstring.push(JSONfragment);
-            })
-        })
+            });
+        });
 
         grunt.file.write(dest, JSON.stringify(JSONstring));
     });
